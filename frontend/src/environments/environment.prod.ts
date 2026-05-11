5765feb6705f189a43bf601f8c445c92839dd977
yosser1234 <yosserbenmahmoud45@gmail.com>
2026-05-12 00:07:05 +0100

    fix gitignore and add missing files

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://192.168.1.175:30808',
+  googleClientId: '131584557440-mk3npcri6s6kbr675teco5qh4p6qerkj.apps.googleusercontent.com',
+  recaptchaClePublique: '6Ldg86EsAAAAAAVgKBeUa095ebgLq2412yXK64TZ'
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+// environment.ts or wherever apiUrl is defined
+  apiUrl: 'http://192.168.1.175:30808',
+  googleClientId: '131584557440-mk3npcri6s6kbr675teco5qh4p6qerkj.apps.googleusercontent.com',
+  recaptchaClePublique: '6Ldg86EsAAAAAAVgKBeUa095ebgLq2412yXK64TZ'
+};
